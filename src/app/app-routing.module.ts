2abcd45f25bc592b6f86fb0ebff16e1944cac820
Yamileth Reveca <yamilethcaldera@gmail.com>
2024-01-02 23:40:33 -0300

    Changelog y comentando componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component'
 import { PeliculasDetallesComponent } from './components/peliculas-detalles/peliculas-detalles.component';
 
 const routes: Routes = [
-
-{
-  path: '',
-  component: HomeComponent
-},
-
-{
-  path: 'detalles/:id',
-  component: PeliculasDetallesComponent
-},
-
-
+  { path: '', component: HomeComponent },
+  { path: 'detalles/:id', component: PeliculasDetallesComponent },
 ];
 
 @NgModule({
